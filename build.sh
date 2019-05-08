ae4c39836308a9078e5a3ebf45268dbc97b07095
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2019-05-09 00:06:39 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,22 +1,51 @@
 #!/bin/bash -eu
 
-./configure
+set -u
+set -e
+set -x
+proj_root="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+
+if [[ -z "${PREFIX:-}" ]]; then
+  PREFIX="$proj_root/install"
+fi
+
+if [[ -z "${CC:-}" ]]; then
+  CC="clang-8"
+fi
+
+if [[ -z "${CXX:-}" ]]; then
+  CXX="clang-8"
+fi
+
+if [[ -z "${CFALGS:-}" ]]; then
+  CFLAGS='-O3'
+fi
+
+if [[ -z "${CXXFLAGS:-}" ]]; then
+  CXXFLAGS='-O3'
+fi
+
+cd "${proj_root}/zlib"
+make clean
+CC="$CC" CFLAGS='-g -fsanitize=fuzzer-no-link' ./configure --prefix $PREFIX
 make -j$(nproc) clean
 make -j$(nproc) all
+make install
+cd "${proj_root}"
 
 # Do not make check as there are tests that fail when compiled with MSAN.
 # make -j$(nproc) check
 
-$CXX $CXXFLAGS -std=c++11 -I. \
-    $SRC/zlib_uncompress_fuzzer.cc -o $OUT/zlib_uncompress_fuzzer \
-    $LIB_FUZZING_ENGINE ./libz.a
+mkdir -p "$PREFIX/bin"
 
-zip $OUT/seed_corpus.zip *.*
+$CXX $CXXFLAGS -g -fsanitize=fuzzer -std=c++11 -I "$PREFIX/include" \
+    "${proj_root}/zlib_uncompress_fuzzer.cc" -o "$PREFIX/bin/zlib_uncompress_fuzzer" \
+    "$PREFIX/lib/libz.a"
 
-for f in $(find $SRC -name '*_fuzzer.c'); do
-    b=$(basename -s .c $f)
-    $CC $CFLAGS -I. $f -c -o /tmp/$b.o
-    $CXX $CXXFLAGS -o $OUT/$b /tmp/$b.o -stdlib=libc++ $LIB_FUZZING_ENGINE ./libz.a
-    rm -f /tmp/$b.o
-    ln -sf $OUT/seed_corpus.zip $OUT/${b}_seed_corpus.zip
+
+for f in $(find "${proj_root}" -name '*_fuzzer.c'); do
+    b="$(basename -s .c "$f")"
+    $CC $CFLAGS -g -fsanitize=fuzzer -I "$PREFIX/include" "$f" -o "$PREFIX/bin/$b" \
+    "$PREFIX/lib/libz.a"
 done
+
